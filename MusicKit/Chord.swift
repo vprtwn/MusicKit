377074df8ae0c697ce20400a29c55cd6d64ecdee
Ben Guo <ben@venmo.com>
2015-01-06 23:44:09 -0500

    Add more pentads

--- MusicKit/Chord.swift
+++ MusicKit/Chord.swift
@@ -36,6 +36,8 @@ public struct Chord : Printable {
         return Chord(intervals: inverted, name: name, inversion: modN)
     }
 
+    public static let Triads : [Chord] = [Chord.MajorTriad,
+    Chord.MinorTriad, Chord.AugmentedTriad, Chord.DiminishedTriad]
     public static let MajorTriad = Chord(intervals: [0, 4, 7],
         name: "Major triad")
     public static let MinorTriad = Chord(intervals: [0, 3, 7],
@@ -45,6 +47,11 @@ public struct Chord : Printable {
     public static let DiminishedTriad = Chord(intervals: [0, 3, 6],
         name: "Diminished triad")
 
+    public static let Tetrads : [Chord] = [Chord.DominantSeventh,
+        Chord.MajorSeventh, Chord.MinorMajorSeventh, Chord.MinorSeventh,
+        Chord.AugmentedMajorSeventh, Chord.AugmentedSeventh,
+        Chord.HalfDiminishedSeventh, Chord.DiminishedSeventh,
+        Chord.DominantSeventhFlatFive]
     public static let DominantSeventh = Chord(intervals: [0, 4, 7, 10],
         name: "Dominant seventh")
     public static let MajorSeventh = Chord(intervals: [0, 4, 7, 11],
@@ -64,25 +71,46 @@ public struct Chord : Printable {
     public static let DominantSeventhFlatFive = Chord(intervals: [0, 4, 6, 10],
         name: "Dominant seventh flat five")
 
-    public static let MajorNinth = Chord(intervals: [0, 4, 7, 11, 14],
-        name: "Major ninth")
+    public static let Pentads : [Chord] = [Chord.DominantNinth,
+        Chord.DominantSeventhFlatNine, Chord.MajorNinth,
+        Chord.MajorSeventhFlatNine, Chord.MinorMajorNinth,
+        Chord.MinorMajorSeventhFlatNine, Chord.MinorNinth,
+        Chord.MinorSeventhFlatNine, Chord.AugmentedMajorNinth,
+        Chord.AugmentedMajorSeventhFlatNine, Chord.AugmentedNinth,
+        Chord.AugmentedSeventhFlatNine, Chord.HalfDiminishedNinth,
+        Chord.HalfDiminishedSeventhFlatNine, Chord.DiminishedNinth,
+        Chord.DiminishedSeventhFlatNine]
     public static let DominantNinth = Chord(intervals: [0, 4, 7, 10, 14],
         name: "Dominant ninth")
+    public static let DominantSeventhFlatNine = Chord(intervals: [0, 4, 7, 10, 13],
+        name: "Dominant seventh flat nine")
+    public static let MajorNinth = Chord(intervals: [0, 4, 7, 11, 14],
+        name: "Major ninth")
+    public static let MajorSeventhFlatNine = Chord(intervals: [0, 4, 7, 11, 13],
+        name: "Major seventh flat nine")
     public static let MinorMajorNinth = Chord(intervals: [0, 3, 7, 11, 14],
-        name: "Dominant ninth")
+        name: "Minor-major ninth")
+    public static let MinorMajorSeventhFlatNine = Chord(intervals: [0, 3, 7, 11, 13],
+        name: "Minor-major seventh flat nine")
     public static let MinorNinth = Chord(intervals: [0, 3, 7, 10, 14],
         name: "Minor ninth")
+    public static let MinorSeventhFlatNine = Chord(intervals: [0, 3, 7, 10, 13],
+        name: "Minor seventh flat nine")
     public static let AugmentedMajorNinth = Chord(intervals: [0, 4, 8, 11, 14],
         name: "Augmented-major ninth")
+    public static let AugmentedMajorSeventhFlatNine = Chord(intervals: [0, 4, 8, 11, 13],
+        name: "Augmented-major seventh flat nine")
     public static let AugmentedNinth = Chord(intervals: [0, 4, 8, 10, 14],
         name: "Augmented ninth")
+    public static let AugmentedSeventhFlatNine = Chord(intervals: [0, 4, 8, 10, 13],
+        name: "Augmented seventh flat nine")
     public static let HalfDiminishedNinth = Chord(intervals: [0, 3, 6, 10, 14],
         name: "Half-diminished ninth")
-    public static let HalfDiminishedMinorNinth = Chord(intervals: [0, 3, 6, 10, 13],
-        name: "Half-diminished minor ninth")
+    public static let HalfDiminishedSeventhFlatNine = Chord(intervals: [0, 3, 6, 10, 13],
+        name: "Half-diminished seventh flat nine")
     public static let DiminishedNinth = Chord(intervals: [0, 3, 6, 9, 14],
         name: "Diminished ninth")
-    public static let DiminishedMinorNinth = Chord(intervals: [0, 3, 6, 9, 13],
-        name: "Diminished minor ninth")
+    public static let DiminishedSeventhFlatNine = Chord(intervals: [0, 3, 6, 9, 13],
+        name: "Diminished seventh flat nine")
 
 }
