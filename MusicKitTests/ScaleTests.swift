89b8a2f1a8969717322e7119a95ffcf6a4011db0
Ben Guo <ben@venmo.com>
2015-05-06 08:46:31 -0400

    clean up scale tests

--- MusicKitTests/ScaleTests.swift
+++ MusicKitTests/ScaleTests.swift
@@ -4,34 +4,18 @@ import MusicKit
 final class ScaleTests: XCTestCase {
     func testMajor() {
         let sut = Scale.Major(Chroma.C*5)
-        XCTAssertEqual(sut,
-            [
-                Chroma.C*5,
-                Chroma.D*5,
-                Chroma.E*5,
-                Chroma.F*5,
-                Chroma.G*5,
-                Chroma.A*5,
-                Chroma.B*5,
-            ])
+        let major : PitchSet = [Chroma.C*5, Chroma.D*5, Chroma.E*5, Chroma.F*5,
+            Chroma.G*5, Chroma.A*5, Chroma.B*5]
+        XCTAssertEqual(sut, major)
+
     }
 
     func testChromatic() {
         let sut = Scale.Chromatic(Chroma.C*5)
-        XCTAssertEqual(sut,
-            [
-                Chroma.C*5,
-                Chroma.Cs*5,
-                Chroma.D*5,
-                Chroma.Ds*5,
-                Chroma.E*5,
-                Chroma.F*5,
-                Chroma.Fs*5,
-                Chroma.G*5,
-                Chroma.Gs*5,
-                Chroma.A*5,
-                Chroma.As*5,
-                Chroma.B*5,
-            ])
+        let chromatic : PitchSet = [Chroma.C*5, Chroma.Cs*5, Chroma.D*5,
+            Chroma.Ds*5, Chroma.E*5, Chroma.F*5, Chroma.Fs*5, Chroma.G*5,
+            Chroma.Gs*5, Chroma.A*5, Chroma.As*5, Chroma.B*5]
+        XCTAssertEqual(sut, chromatic)
+
     }
 }
