8799935760faaabc1e8acd89e21150f5862e0f7a
Ben Guo <ben@venmo.com>
2015-05-07 07:44:42 -0400

    add extended slash chords

--- MusicKit/ChordName.swift
+++ MusicKit/ChordName.swift
@@ -4,6 +4,14 @@ import Foundation
 
 /// defines arrays of (Harmonizer, ChordTuple) tuples
 struct ChordTuples {
+    static let Additions = [
+        ChordAddition.FlatNine,
+        ChordAddition.Nine,
+        ChordAddition.SharpEleven,
+        ChordAddition.FlatThirteen,
+        ChordAddition.Thirteen,
+    ]
+
     static let Triads = [
         (Chord.Major, Chord._Major),
         (Chord.Minor, Chord._Minor),
@@ -44,43 +52,77 @@ extension Chord {
         var pitchSet = pitchSet
         pitchSet.normalize()
         let count = pitchSet.count
+        let gamutCount = pitchSet.gamut().count
 
-        // must have at least a triad after normalization
-        if count < 3 { return nil }
+        // early return if:
+        // - less than a triad after normalization
+        // - one or more pitch is chroma-less
+        if count < 3 || count != gamutCount { return nil }
 
-        // MARK: Triads
-        if count < 5 {
-            return nameWithoutExtensions(pitchSet)
+        let bass = pitchSet.first()!
+        let bassName = bass.chroma?.description
+        var removedBass = pitchSet
+        removedBass.remove(bass)
+
+        // Triads
+        if count == 3 {
+            return _name(pitchSet, tuples: ChordTuples.Triads, includeSlash: true)
+        }
+        // Tetrads
+        else if count == 4 {
+            let nameOpt = _name(pitchSet, tuples: ChordTuples.Tetrads, includeSlash: true)
+            if let name = nameOpt {
+                return name
+            }
+            // remove bass note and attempt to form slash chord
+            else {
+                let topNameOpt = _name(removedBass, tuples: ChordTuples.Triads, includeSlash: false)
+                if let name = topNameOpt {
+                    return bassName.map { "\(name)/\($0)" }
+                }
+            }
+        }
+        // Pentads
+        else if count == 5 {
+            // remove bass note and attempt to form slash chord
+            let topNameOpt = _name(removedBass, tuples: ChordTuples.Tetrads, includeSlash: false)
+            if let name = topNameOpt {
+                return bassName.map { "\(name)/\($0)" }
+            }
         }
         return nil
     }
 
-    static func nameWithoutExtensions(pitchSet: PitchSet) -> String? {
+    static func _name(pitchSet: PitchSet,
+        tuples: [(Harmonizer, ChordTuple)],
+        includeSlash: Bool) -> String? {
         let count = pitchSet.count
-        let root = pitchSet.first()!
+        let bass = pitchSet.first()!
         let indices = pitchSet.semitoneIndices()
-        let chordTuples = ChordTuples.Triads + ChordTuples.Tetrads
         // check root position chords
-        for tuple in chordTuples {
+        for tuple in tuples {
             let shortSuffix = tuple.1.2
-            var _indices = tuple.0(root).semitoneIndices()
+            var _indices = tuple.0(bass).semitoneIndices()
             if _indices == indices {
-                let rootName = root.chroma?.description
+                let rootName = bass.chroma?.description
                 return rootName.map { "\($0)\(shortSuffix)" }
             }
         }
         // check inversions
-        for tuple in chordTuples {
+        for tuple in tuples {
             let shortSuffix = tuple.1.2
-            let _pitchSet = tuple.0(root)
-            var _indices = tuple.0(root).semitoneIndices()
+            let _pitchSet = tuple.0(bass)
+            var _indices = tuple.0(bass).semitoneIndices()
             for i in 1..<count {
                 let inversion = MKUtil.zero(MKUtil.invert(_indices, n: UInt(i)))
                 if inversion == indices {
                     let rootName = pitchSet[count - i].chroma?.description
-                    let bottomNameOpt = pitchSet[0].chroma?.description
-                    if let bottomName = bottomNameOpt {
-                        return rootName.map { "\($0)\(shortSuffix)/\(bottomName)" }
+                    let baseNameOpt = includeSlash ? pitchSet[0].chroma?.description : nil
+                    if let baseName = baseNameOpt {
+                        return rootName.map { "\($0)\(shortSuffix)/\(baseName)" }
+                    }
+                    else {
+                        return rootName.map { "\($0)\(shortSuffix)" }
                     }
                 }
             }
--- MusicKitTests/ChordNameExtendedTests.swift
+++ MusicKitTests/ChordNameExtendedTests.swift
@@ -2,5 +2,27 @@ import XCTest
 import MusicKit
 
 final class ChordNameExtendedTests: XCTestCase {
+    func testTriadSlashChords() {
+        var sut : PitchSet = [Chroma.Cs*0, Chroma.E*2, Chroma.G*3, Chroma.C*4]
+        var name = Chord.name(sut)
+        var expected = "CM/C♯"
+        XCTAssert(name == expected, "\(name) != \(expected)")
 
+        sut = [Chroma.Fs*0, Chroma.Ds*2, Chroma.G*3, Chroma.C*4]
+        name = Chord.name(sut)
+        expected = "Cm/F♯"
+        XCTAssert(name == expected, "\(name) != \(expected)")
+    }
+
+    func testTetradSlashChords() {
+        var sut : PitchSet = [Chroma.Cs*0, Chroma.E*2, Chroma.G*3, Chroma.C*4]
+        var name = Chord.name(sut)
+        var expected = "CM/C♯"
+        XCTAssert(name == expected, "\(name) != \(expected)")
+
+        sut = [Chroma.Fs*0, Chroma.Ds*2, Chroma.G*3, Chroma.C*4]
+        name = Chord.name(sut)
+        expected = "Cm/F♯"
+        XCTAssert(name == expected, "\(name) != \(expected)")
+    }
 }
