9de8565521a192e79d709d88a185f6aefa5c31ec
Ben Guo <ben@venmo.com>
2015-05-14 21:43:55 -0400

    add interval

---
+++ MusicKit/Interval.swift
@@ -0,0 +1,30 @@
+//  Copyright (c) 2015 Ben Guo. All rights reserved.
+
+import Foundation
+
+public enum Interval : UInt {
+    case Unison = 0
+    case MinorSecond = 1
+    case MajorSecond = 2
+    case MinorThird = 3
+    case MajorThird = 4
+    case PerfectFourth = 5
+    case Tritone = 6
+    case PerfectFifth = 7
+    case MinorSixth = 8
+    case MajorSixth = 9
+    case MinorSeventh = 10
+    case MajorSeventh = 11
+    case Octave = 12
+    case MinorNinth = 13
+    case MajorNinth = 14
+    case MinorTenth = 15
+    case MajorTenth = 16
+    case PerfectEleventh = 17
+    case Tritave = 18
+    case AugmentedEleventh = 19
+    case MinorThirteenth = 20
+    case MajorThirteenth = 21
+    case MinorFourteenth = 22
+    case MajorFourteenth = 23
+}
